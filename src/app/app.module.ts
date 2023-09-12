5c2b7f5f480bcbab711985c7d18cca3783fd4da3
Bigzik10 <niksavage5@gmail.com>
2023-09-13 00:13:56 +0100

    xik

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { LoginComponent } from './login/login.component';
 import { PagesComponent } from './settings/pages/pages.component';
 
 import { DataTablesModule } from 'angular-datatables';
+import { PaymentTableComponent } from './payment/payment-table/payment-table.component';
 // import { ChartsModule } from 'ng2-charts';
 // import { ChartsModule } from 'ng2-charts';
 
@@ -39,6 +40,7 @@ import { DataTablesModule } from 'angular-datatables';
     SettlementsTableComponent,
     LoginComponent,
     PagesComponent,
+    PaymentTableComponent,
   ],
   imports: [
     BrowserModule,
